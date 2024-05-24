bce5c571e37a40e6773b580c5302106352f2ac71
Subh0048 <subhamkumarsahu99@gmail.com>
2024-05-24 23:22:44 +0530

    setup express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv"
+import app from "./app.js";
 
 import DB_CONNECT from "./db/index.js";
 
@@ -6,4 +7,15 @@ dotenv.config({
   path: "./env",
 });
 
-DB_CONNECT();
+DB_CONNECT()
+.then(()=>{
+ 
+  
+app.listen(process.env.PORT,()=>{
+  console.log(`server is running on port no ${process.env.PORT}`);
+})
+  
+})
+.catch((err)=>{
+  console.log("monodb connection failed ",err);
+})
