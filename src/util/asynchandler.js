44ae9bd9664d96e440aeedcf2c23f02b7890e461
Subh0048 <subhamkumarsahu99@gmail.com>
2024-05-25 12:58:32 +0530

    util,api response and error handling

---
+++ src/util/asynchandler.js
@@ -0,0 +1,6 @@
+const asynchandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asynchandler };
